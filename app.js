de80f705c6e3344dd5838be1542ef4436959c8e5
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-02 01:36:04 +0300

    static files added

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  res.status(200).res.send('index page');
+  //   res.status(200).res.send('index page');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  //   res.status(200).res.send('index page');
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const port = 3000;
