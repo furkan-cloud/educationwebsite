b7a432f95c903b387b1359c213c118458385f550
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-09 18:02:03 +0300

    dashboard feature added

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ exports.loginUser = (req, res) => {
             // USER SESSION
 
             req.session.userID = user._id;
-            return res.status(200).redirect('/');
+            return res.status(200).redirect('/users/dashboard');
           }
         });
       }
@@ -40,5 +40,13 @@ exports.loginUser = (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
