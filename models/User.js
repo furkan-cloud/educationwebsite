fa61932901bd5cf1e6136d4535680224a13c3b45
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-09 22:20:27 +0300

    roles added

--- models/User.js
+++ models/User.js
@@ -16,14 +16,19 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'admin'],
+    default: 'student',
+  },
 });
 
 UserSchema.pre('save', function (next) {
   const user = this;
   bcrypt.hash(user.password, 10, (error, hash) => {
-      user.password = hash;
-      next()
-  })
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model('User', UserSchema);
