261edcaf94d53d671ae9794b4dad33f970c577dc
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-10 11:09:29 +0300

    search feature added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -23,13 +23,29 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
     const category = await Category.findOne({ slug: categorySlug });
     let filter = {};
     if (categorySlug) {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    if (query) {
+      filter = { name: query };
+    }
+
+    if (!query && !categorySlug) {
+      (filter.name = ''), (filter.category = null);
+    }
+
+    const courses = await Course.find({
+      $or: [
+        { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+        { category: filter.category },
+      ],
+    })
+      .sort('-createdAt')
+      .populate('user');
     const categories = await Category.find();
     // res.status(200).json({
     //   status: 'success',
@@ -50,10 +66,12 @@ exports.getCourse = async (req, res) => {
     const slug = req.params.slug;
     const user = await User.findById(req.session.userID);
     const course = await Course.findOne({ slug: slug }).populate('user');
+    const categories = await Category.find();
 
     res.status(200).render('course', {
       course,
       user,
+      categories,
       page_name: 'course',
     });
   } catch (error) {
