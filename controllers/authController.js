24c126911f518fe73a9c3462d497ce8e444307bd
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-10 23:33:56 +0300

    login and register validations added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Course = require('../models/Course');
@@ -12,26 +13,45 @@ exports.createUser = async (req, res) => {
     //   user,
     // });
   } catch (error) {
-    res.status(400).json({ status: 'failed', error });
+    const errors = validationResult(req);
+    console.log(errors.array()[0].msg);
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash('error', `${errors.array()[i].msg}`);
+    }
+
+    res.status(400).redirect('/register');
+    // res.status(400).json({ status: 'failed', error });
   }
 };
 
+// async await varken hata veriyor ve çalışmıyor?
 exports.loginUser = (req, res) => {
   try {
     const { email, password } = req.body;
-
+    console.log('1');
     User.findOne({ email }, (err, user) => {
       if (user) {
+        console.log('2');
         bcrypt.compare(password, user.password, (err, same) => {
-            // USER SESSION
-
+          // USER SESSION
+          if (same) {
             req.session.userID = user._id;
-            return res.status(200).redirect('/users/dashboard');
+            res.status(200).redirect('/users/dashboard');
+          } else {
+            console.log('3');
+            req.flash('error', `Your password is not correct`);
+            return res.status(400).redirect('/login');
+          }
         });
+      } else {
+        console.log('4');
+        req.flash('error', `User is not exist`);
+        return res.status(400).redirect('/login');
       }
     });
   } catch (error) {
-    res.status(400).json({
+    console.log('5');
+    return res.status(400).json({
       status: 'fail',
       error,
     });
